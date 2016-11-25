99c9cc61d9c1a48775d759f58458d41fa5951f99
Cesar <alex.ibarra.roque@hotmail.com>
2016-11-24 23:03:11 -0600

    Create Proyecto.java

---
+++ PROYECTO/Proyecto.java
@@ -0,0 +1,22 @@
+package proyecto;
+
+import VISTA.frmPrincipal;
+import VISTA.frmUsuario;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        frmPrincipal in = new frmPrincipal();
+//        in.setVisible(true);
+ new frmPrincipal() {}.setVisible(true);
+    }
+    
+}
+
